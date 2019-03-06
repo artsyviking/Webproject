3d815eea66cf593af3d3f5734c2e2aee0ce3c7f6
artsyviking <30381532+artsyviking@users.noreply.github.com>
2019-03-05 18:28:53 -0800

    got it working

---
+++ main.js
@@ -0,0 +1,51 @@
+function npc() {
+    let play = Math.ceil(Math.random() * 3);
+    return play;
+}
+
+/*
+Paper = 1
+Rock = 2
+Scissors = 3
+*/
+function compare(playerChoice) {
+    let npcChoice = this.npc();
+
+    if (playerChoice === npcChoice) {
+        this.outputResults('This round ends in a draw!');
+        //This is a draw!
+    }
+    else if (playerChoice === 3 && npcChoice === 1) {
+        this.outputResults('You won this round!');
+        //Player chose scissors, npc chose paper. Player wins.
+    }
+    else if (playerChoice === 3 && npcChoice === 2) {
+        this.outputResults('You lost this round...')
+        //Player chose scissors, npc chose rock. Npc wins.
+    }
+    else if (playerChoice === 2 && npcChoice === 3) {
+        this.outputResults('You won this round!');
+        //Player chose rock, npc chose scissors. Player wins.
+    }
+    else if (playerChoice === 2 && npcChoice === 1) {
+        this.outputResults('You lost this round...');
+        //Player chose rock, npc chose paper. Npc wins.
+    }
+    else if (playerChoice === 1 && npcChoice === 2) {
+        this.outputResults('You won this round!');
+        //Player chose paper, npc chose rock. Player wins.
+    }
+    else if (playerChoice === 1 && npcChoice === 3) {
+        this.outputResults('You lost this round...');
+        //Player chose paper, npc chose scissors. Npc wins.
+    }
+    else {
+        this.outputResults('You somehow broke the game. gg');
+        //You broke the game. gg.
+    }
+}
+
+function outputResults(result) {
+    let outcomeElement = document.getElementById('outcomeText');
+    outcomeElement.innerText = result;
+}
