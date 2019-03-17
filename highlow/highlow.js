3f67e824236b8699ac7b8dbd7179b77a3a1aa4c6
artsyviking <30381532+artsyviking@users.noreply.github.com>
2019-03-17 04:52:13 -0700

    u

--- highlow/highlow.js
+++ highlow/highlow.js
@@ -9,10 +9,41 @@ function hundred() {
     }
 }
 
+
 function numberGen() {
     let play = Math.ceil(Math.random() * 100);
     return play;
 }
 
+function numberClick() {
+    addEventListener(document.id(this.i.toString), this.i);
+}
+
+function addEventListener(id, num) {
+    let highLowElement = document.getElementById(id);
+    highLowElement.addEventListener('click', (event) => {
+        compareWithDelay(num);
+    });
+}
+
+function compareWithDelay(playerChoice) {
+    setTimeout(() => {
+        compare(playerChoice);
+    }, 1000);
+}
+
+function compare(playerChoice) {
+    let npcChoice = this.numberGen();
+    if (playerChoice < npcChoice) {
+
+    }
+}
+
+function outputResults(result) {
+    let outcomeElement = document.getElementById('outcomeText');
+    outcomeElement.innerText = result;
+}
 
 window.onload = () => hundred();
+
+numberClick();
