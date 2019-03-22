a96d70c1397385e72503faf2cd31c8c3895fc342
artsyviking <30381532+artsyviking@users.noreply.github.com>
2019-03-22 03:13:58 -0700

    update

--- janken/janken.js
+++ janken/janken.js
@@ -66,6 +66,7 @@ function compare(playerChoice) {
 }
 
 function outputResults(result) {
+    document.getElementById('loadingGif').removeAttribute('src');
     let outcomeElement = document.getElementById('outcomeText');
     outcomeElement.innerText = result;
 }
